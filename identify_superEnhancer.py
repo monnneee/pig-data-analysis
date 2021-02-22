00020e6d982c51e58c4731451eac8f6a3fafb431
monnneee <66565200+monnneee@users.noreply.github.com>
2021-02-22 13:56:50 +0800

    Update identify_superEnhancer.py

--- identify_superEnhancer.py
+++ identify_superEnhancer.py
@@ -2,7 +2,7 @@
 import argparse
 import os
 parser = argparse.ArgumentParser(description="description:identify superenhancer")
-parser.add_argument("--cong",help="congifure table") #configure file is the same as CRE_merge.py
+parser.add_argument("--cong",help="congifure table") #5 column configure file inlcuding breed, tissue, enhancer.bed.path, H3K27ac.bam.path,	and Input.bam.path.
 parser.add_argument("--genome",help="SS10")
 args=parser.parse_args()
 dict1={}
@@ -12,7 +12,7 @@ with open(args.cong, 'rt') as file1:
         line1=line1.rstrip()
         list1=line1.split("\t")
         dict1[list1[0]+'-'+list1[1]]= list1[2]
-        dict2[list1[0]+'-'+list1[1]]= list1[6]+"\t"+list1[7]
+        dict2[list1[0]+'-'+list1[1]]= list1[3]+"\t"+list1[4]
     for key in dict1.keys():
         cmd="awk '{{print $1"'"\t"'"NR"'"\t"'""'"enhancer"'""'"\t"'"$2"'"\t"'"$3"'"\t"'"$4"'"\t"'""'"."'""'"\t"'""'"."'""'"\t"'"NR}}' {} > {}-enhancer.gff".format(dict1[key],key)
         os.system(cmd)
